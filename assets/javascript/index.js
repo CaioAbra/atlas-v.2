f052f46a97afaa69fbbb9121f7d786d78c559dc1
CaioAbra <caioabra41@gmail.com>
2024-08-25 22:40:42 -0300

    navbar

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -90,3 +90,13 @@ $(document).ready(function () {
 
     $(".carousel").hover(stopAutoSlide, startAutoSlide); // Pausa o movimento ao passar o mouse sobre o carousel
 });
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const navBar = document.getElementById('nav');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    navBar.classList.toggle('active');
+});
